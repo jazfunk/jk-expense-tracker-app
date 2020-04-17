998bae05ed71811f87a79a4f2521912648dfcd8c
Jeff King <jazfunk@gmail.com>
2020-04-17 16:51:51 -0400

    2nd commit.  Local storage, and Expense object array working correctly.

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-var expenses = [];
-
 class Expense {
   constructor(id, date, description, amount, vendor) {
     this.id = id;
@@ -10,26 +8,49 @@ class Expense {
   }
 }
 
-let addNewExpense = (date, description, amount, vendor) => {
-  const id = Date.now() + Math.random();
-  const newExpense = new Expense(id, date, description, amount, vendor);
-  debugger;
-  expenses.push(newExpense);
-  console.table(expenses);
-};
 
-document.getElementById("add-expense-btn").addEventListener("click", (e) => {
+document.querySelector("form").addEventListener("click", (e) => {
   e.preventDefault;
-  debugger;
   addNewExpense("3/15/2020", "desc text", "2.22", "McDonalds");
 });
 
-function updateExistingExpenses(expense) {
+const setDefaultInputValues = () => {
+    document.getElementById('expense-date').value = "Date of Expense";
+    document.getElementById('expense-description').value = "Description";
+    document.getElementById('expense-amount').value = "Amount of Expense";
+    document.getElementById('expense-vendor').value = "Vendor";
+}
+
+window.onload = () => {
+  // Temporary until this part is built propertly //
+  getLocalExpenses(window.localStorage.getItem("expenses"));
+  setDefaultInputValues();
+  // End of temp code //
+};
+
+const getLocalExpenses = (expenses) => {
+  var mainContentCell = document.querySelector(".main-cell");
+  mainContentCell.innerText = expenses;
+};
+
+const addNewExpense = (date, description, amount, vendor) => {
+  const id = Date.now() + Math.random();
+  const newExpense = new Expense(id, date, description, amount, vendor);
+  setLocalExpenses(newExpense);
+};
+
+const setLocalExpenses = (newExpense) => {
+  const savedExpenses = JSON.parse(window.localStorage.getItem("expenses")) || [];
+  savedExpenses.push(newExpense);
+  window.localStorage.setItem("expenses", JSON.stringify(savedExpenses));
+};
+
+const updateExistingExpense = (expense) => {
   const savedExpenses = JSON.parse(localStorage.getItem("expenses")) || [];
-  const matchingExpense = expenses.find(function (savedExpenses) {
-    return expense.id === savedExpenses.id;
-  });
+  const matchingExpense = savedExpenses.find(
+    (savedExpense) => expense.id === savedExpenses.id
+  );
   const index = savedExpenses.indexOf(matchingExpense);
   savedExpenses[index] = expense;
   window.localStorage.setItem("expenses", JSON.stringify(savedExpenses));
-}
+};
