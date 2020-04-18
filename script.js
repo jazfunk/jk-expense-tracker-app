c836790ac3a888414caba93c1f0c6d630d42e61b
Jeff King <jazfunk@gmail.com>
2020-04-18 14:41:33 -0400

    Updates to styles.

--- script.js
+++ script.js
@@ -18,22 +18,14 @@ function validateFormValues() {
     const date = document.getElementById('expense-date').value;
     const description = document.getElementById('expense-description').value;
     const amount = document.getElementById('expense-amount').value;
-    const vendor = document.getElementById('expense-vendor').value;
-    
+    const vendor = document.getElementById('expense-vendor').value;    
     addNewExpense(date, description, amount, vendor);
 }
-
-const setDefaultInputValues = () => {
-  document.getElementById("expense-date").value = "";
-  document.getElementById("expense-description").value = "";
-  document.getElementById("expense-amount").value = "";
-  document.getElementById("expense-vendor").value = "";
-};
 
 window.onload = () => {
   const savedExpenses = JSON.parse(localStorage.getItem("expenses")) || [];
   displayExpenses(savedExpenses);
-  setDefaultInputValues();
+  document.getElementById('expense-date').focus();  
 };
 
 const addNewExpense = (date, description, amount, vendor) => {
@@ -70,13 +62,30 @@ function displayExpenses(expenses) {
   html += "<td class='expense-header-row'><input id='delete-all' type='checkbox' class='check-box'></td></tr>";
   for (var i = 0; i < expenses.length; i++) {
     html += "<tr>";
-    html += "<td class='expense-data-row'>" + expenses[i].vendor + "</td>";
-    html += "<td class='expense-data-row'>" + expenses[i].description + "</td>";
-    html += "<td class='expense-data-row'>" + expenses[i].date + "</td>";
-    html += "<td class='expense-data-row'>" + expenses[i].amount + "</td>";
-    html += "<td class='expense-data-row'><input type='checkbox' class='delete-checkbox'></td>";
+    html += "<td class='expense-data-row vendor-format'>" + expenses[i].vendor + "</td>";
+    html += "<td class='expense-data-row description-format'>" + expenses[i].description + "</td>";
+    html += "<td class='expense-data-row  date-format'>" + displayDate(expenses[i].date) + "</td>";
+    html += "<td class='expense-data-row enMoney'>" + displayCurrency(expenses[i].amount) + "</td>";
+    html += "<td class='expense-data-row checkbox-format'><input type='checkbox' class='delete-checkbox'></td>";
     html += "</tr>";
   }
   html += "</table>";
   document.getElementById("display-expenses").innerHTML = html;
+}
+
+const displayCurrency = (value) => {
+    const dec = value.split('.')[1]
+    const len = dec && dec.length > 2 ? dec.length : 2;
+    return numberWithCommas(Number(value).toFixed(len));
+}
+
+const numberWithCommas = (x) => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+const displayDate = (value) => {
+    const d = new Date(value);
+    const month = new Intl.DateTimeFormat('en', {month: 'numeric'}).format(d);
+    const day = new Intl.DateTimeFormat('en', {day: '2-digit'}).format(d);    
+    const year = new Intl.DateTimeFormat('en', {year: 'numeric' }).format(d);
+    const dateFormtted = `${month}-${day}-${year}`;
+    return dateFormtted;
 }
