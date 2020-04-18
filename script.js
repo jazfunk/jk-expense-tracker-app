af3d9f596e23050193119be85e74f5ec786d410f
Jeff King <jazfunk@gmail.com>
2020-04-18 17:16:18 -0400

    More style and subtle function changes.

--- script.js
+++ script.js
@@ -9,27 +9,48 @@ class Expense {
 }
 
 const form = document.querySelector("form");
-form.addEventListener("submit", (e) => {
-  //e.preventDefault();
-  validateFormValues();
-});
+form.addEventListener("submit", (e) => validateFormValues());
+
+// This doesn't work
+const checkedCheckBox = document.querySelector("delete-checkbox");
+if (checkedCheckBox) {
+  checkedCheckBox.addEventListener("change", (e) => {
+    if (e.target.checked) {
+      alert("checked");
+    }
+  });
+}
 
 function validateFormValues() {
-    const date = document.getElementById('expense-date').value;
-    const description = document.getElementById('expense-description').value;
-    const amount = document.getElementById('expense-amount').value;
-    const vendor = document.getElementById('expense-vendor').value;    
-    addNewExpense(date, description, amount, vendor);
+  const date = document.getElementById("expense-date").value;
+  const description = document.getElementById("expense-description").value;
+  const amount = document.getElementById("expense-amount").value;
+  const vendor = document.getElementById("expense-vendor").value;
+  addNewExpense(date, description, amount, vendor);
 }
+
+const compareDates = (a, b) => {
+  const dateA = a.date;
+  const dateB = b.date;
+
+  let comparison = 0;
+  if (dateA > dateB) {
+    comparison = 1;
+  } else if (dateA < dateB) {
+    comparison = -1;
+  }
+
+  return comparison;
+};
 
 window.onload = () => {
   const savedExpenses = JSON.parse(localStorage.getItem("expenses")) || [];
-  displayExpenses(savedExpenses);
-  document.getElementById('expense-date').focus();  
+  displayExpenses(savedExpenses.sort(compareDates));
+  document.getElementById("expense-date").focus();
 };
 
 const addNewExpense = (date, description, amount, vendor) => {
-  const id = Date.now() + Math.random(); 
+  const id = Date.now() + Math.random();
   const newExpense = new Expense(id, date, description, amount, vendor);
   setLocalExpenses(newExpense);
 };
@@ -52,21 +73,47 @@ const updateExistingExpense = (expense) => {
   window.localStorage.setItem("expenses", JSON.stringify(savedExpenses));
 };
 
+const deleteExpense = (expense) => {
+  const savedExpenses = JSON.parse(localStorage.getItem("expenses")) || [];
+  const matchingExpense = savedExpenses.find(
+    (savedExpense) => expense.id === savedExpenses.id
+  );
+  const index = savedExpenses.indexOf(matchingExpense);
+  if (index > -1) {
+    savedExpenses.splice(index, 1);
+    localStorage.setItem("expenses", JSON.stringify(savedExpenses));
+  }
+};
+
 function displayExpenses(expenses) {
-  var html = "<table border class='expenses-table'>"
+  var html = "<table border class='expenses-table'>";
   html += "<tr>";
   html += "<td class='expense-header-row'>Vendor</td>";
   html += "<td class='expense-header-row'>Description</td>";
   html += "<td class='expense-header-row'>Date</td>";
-  html += "<td class='expense-header-row'>Amount</td>";
-  html += "<td class='expense-header-row'><input id='delete-all' type='checkbox' class='check-box'></td></tr>";
+  html += "<td class='expense-header-row'>$</td>";
+  html +=
+    "<td class='expense-header-row'><input id='delete-all' type='checkbox' class='check-box'></td></tr>";
   for (var i = 0; i < expenses.length; i++) {
     html += "<tr>";
-    html += "<td class='expense-data-row vendor-format'>" + expenses[i].vendor + "</td>";
-    html += "<td class='expense-data-row description-format'>" + expenses[i].description + "</td>";
-    html += "<td class='expense-data-row  date-format'>" + displayDate(expenses[i].date) + "</td>";
-    html += "<td class='expense-data-row enMoney'>" + displayCurrency(expenses[i].amount) + "</td>";
-    html += "<td class='expense-data-row checkbox-format'><input type='checkbox' class='delete-checkbox'></td>";
+    html +=
+      "<td class='expense-data-row vendor-format'>" +
+      expenses[i].vendor +
+      "</td>";
+    html +=
+      "<td class='expense-data-row description-format'>" +
+      expenses[i].description +
+      "</td>";
+    html +=
+      "<td class='expense-data-row  date-format'>" +
+      displayDate(expenses[i].date) +
+      "</td>";
+    html +=
+      "<td class='expense-data-row enMoney'>" +
+      displayCurrency(expenses[i].amount) +
+      "</td>";
+    html +=
+      "<td class='expense-data-row checkbox-format'><input type='checkbox' class='delete-checkbox'></td>";
     html += "</tr>";
   }
   html += "</table>";
@@ -74,18 +121,18 @@ function displayExpenses(expenses) {
 }
 
 const displayCurrency = (value) => {
-    const dec = value.split('.')[1]
-    const len = dec && dec.length > 2 ? dec.length : 2;
-    return numberWithCommas(Number(value).toFixed(len));
-}
+  const dec = value.split(".")[1];
+  const len = dec && dec.length > 2 ? dec.length : 2;
+  return addCommasToDollars(Number(value).toFixed(len));
+};
 
-const numberWithCommas = (x) => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+const addCommasToDollars = (x) => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
 const displayDate = (value) => {
-    const d = new Date(value);
-    const month = new Intl.DateTimeFormat('en', {month: 'numeric'}).format(d);
-    const day = new Intl.DateTimeFormat('en', {day: '2-digit'}).format(d);    
-    const year = new Intl.DateTimeFormat('en', {year: 'numeric' }).format(d);
-    const dateFormtted = `${month}-${day}-${year}`;
-    return dateFormtted;
-}
+  const d = new Date(value);
+  const month = new Intl.DateTimeFormat("en", { month: "numeric" }).format(d);
+  const day = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(d);
+  const year = new Intl.DateTimeFormat("en", { year: "numeric" }).format(d);
+  const dateFormtted = `${month}-${day}-${year}`;
+  return dateFormtted;
+};
