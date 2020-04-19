dcec445666a6a44acc2be74393dee532995c027f
Jeff King <jazfunk@gmail.com>
2020-04-18 20:33:05 -0400

    Done for the day and pushing up my changes, mostly styling.

---
+++ expense.js
@@ -0,0 +1,9 @@
+class Expense {
+    constructor(id, date, description, amount, vendor) {
+      this.id = id;
+      this.date = date;
+      this.description = description;
+      this.amount = amount;
+      this.vendor = vendor;
+    }
+  }
--- script.js
+++ script.js
@@ -8,46 +8,47 @@ class Expense {
   }
 }
 
-const form = document.querySelector("form");
-form.addEventListener("submit", (e) => validateFormValues());
-
-// This doesn't work
-const checkedCheckBox = document.querySelector("delete-checkbox");
-if (checkedCheckBox) {
-  checkedCheckBox.addEventListener("change", (e) => {
-    if (e.target.checked) {
-      alert("checked");
-    }
-  });
-}
-
-function validateFormValues() {
-  const date = document.getElementById("expense-date").value;
-  const description = document.getElementById("expense-description").value;
-  const amount = document.getElementById("expense-amount").value;
-  const vendor = document.getElementById("expense-vendor").value;
-  addNewExpense(date, description, amount, vendor);
-}
-
-const compareDates = (a, b) => {
-  const dateA = a.date;
-  const dateB = b.date;
-
-  let comparison = 0;
-  if (dateA > dateB) {
-    comparison = 1;
-  } else if (dateA < dateB) {
-    comparison = -1;
-  }
-
-  return comparison;
-};
-
 window.onload = () => {
+  checkForUndo();
   const savedExpenses = JSON.parse(localStorage.getItem("expenses")) || [];
   displayExpenses(savedExpenses.sort(compareDates));
   document.getElementById("expense-date").focus();
 };
+
+const checkForUndo = () => {
+  const undoLink = document.getElementById("undo-message");
+  const undoDisplayText = "Expenses Deleted, click here to restore.";
+  const deletedExpenses = localStorage.getItem("tempExpenses") || [];
+  undoLink.textContent = deletedExpenses.length > 0 ? undoDisplayText : "";
+};
+
+const undoLink = document
+  .getElementById("undo-message")
+  .addEventListener("click", (e) => {
+    const tempExpenses = localStorage.getItem("tempExpenses");
+    localStorage.setItem("expenses", tempExpenses);
+    localStorage.removeItem("tempExpenses");
+    location.reload();
+  });
+
+const clearButton = document
+  .getElementById("clear-expenses-btn")
+  .addEventListener("click", () => {
+    const tempExpenses = localStorage.getItem("expenses");
+    localStorage.clear();
+    localStorage.setItem("tempExpenses", tempExpenses);
+    location.reload();
+  });
+
+const form = document
+  .getElementById("expense-form")
+  .addEventListener("submit", () => {
+    const date = document.getElementById("expense-date").value;
+    const description = document.getElementById("expense-description").value;
+    const amount = document.getElementById("expense-amount").value;
+    const vendor = document.getElementById("expense-vendor").value;
+    addNewExpense(date, description, amount, vendor);
+  });
 
 const addNewExpense = (date, description, amount, vendor) => {
   const id = Date.now() + Math.random();
@@ -55,12 +56,23 @@ const addNewExpense = (date, description, amount, vendor) => {
   setLocalExpenses(newExpense);
 };
 
+const compareDates = (a, b) => {
+  let comparison = 0;
+  if (a.date > b.date) {
+    comparison = -1;
+  } else if (a.date < b.date) {
+    comparison = 1;
+  }
+  return comparison;
+};
+
 const setLocalExpenses = (newExpense) => {
   const savedExpenses =
     JSON.parse(window.localStorage.getItem("expenses")) || [];
   savedExpenses.push(newExpense);
   displayExpenses(savedExpenses);
-  window.localStorage.setItem("expenses", JSON.stringify(savedExpenses));
+  localStorage.setItem("expenses", JSON.stringify(savedExpenses));
+  localStorage.removeItem("tempExpenses");
 };
 
 const updateExistingExpense = (expense) => {
@@ -85,7 +97,27 @@ const deleteExpense = (expense) => {
   }
 };
 
-function displayExpenses(expenses) {
+const displayCurrency = (value) => {
+  const dec = value.split(".")[1];
+  const len = dec && dec.length > 2 ? dec.length : 2;
+  return addCommasToDollars(Number(value).toFixed(len));
+};
+
+const addCommasToDollars = (value) =>
+  value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+const displayDate = (value) => {
+  const d = new Date(value);
+  const month = new Intl.DateTimeFormat("en", { month: "numeric" }).format(d);
+  const day = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(d);
+  const year = new Intl.DateTimeFormat("en", { year: "numeric" }).format(d);
+  const dateFormtted = `${month}-${day}-${year}`;
+  return dateFormtted;
+};
+
+// Refactor this using document.createElement()
+// .innerHTML is potentially unsafe
+const displayExpenses = (expenses) => {
   var html = "<table border class='expenses-table'>";
   html += "<tr>";
   html += "<td class='expense-header-row'>Vendor</td>";
@@ -118,21 +150,4 @@ function displayExpenses(expenses) {
   }
   html += "</table>";
   document.getElementById("display-expenses").innerHTML = html;
-}
-
-const displayCurrency = (value) => {
-  const dec = value.split(".")[1];
-  const len = dec && dec.length > 2 ? dec.length : 2;
-  return addCommasToDollars(Number(value).toFixed(len));
-};
-
-const addCommasToDollars = (x) => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-
-const displayDate = (value) => {
-  const d = new Date(value);
-  const month = new Intl.DateTimeFormat("en", { month: "numeric" }).format(d);
-  const day = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(d);
-  const year = new Intl.DateTimeFormat("en", { year: "numeric" }).format(d);
-  const dateFormtted = `${month}-${day}-${year}`;
-  return dateFormtted;
 };
