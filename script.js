e9846e4740ebdb39d7ccd5170756580f8461d9e2
Jeff King <jazfunk@gmail.com>
2020-04-20 16:16:06 -0400

    Removed tables, now using createElement.  Delete not working.

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 //   }
 // }
 class Expense {
-  constructor(id, date, description, amount, vendor) {
+  constructor(vendor, description, date, amount, id) {
     this.vendor = vendor;
     this.description = description;
     this.date = date;
@@ -20,25 +20,9 @@ class Expense {
 window.onload = () => {
   checkForUndo();
   const savedExpenses = JSON.parse(localStorage.getItem("expenses")) || [];
-  displayExpensesNEW(savedExpenses.sort(compareDates));
+  displayExpenses(savedExpenses.sort(compareDates));
   document.getElementById("expense-date").focus();
 };
-
-const checkForUndo = () => {
-  const undoLink = document.getElementById("undo-message");
-  const undoDisplayText = "Expenses Deleted, click here to restore.";
-  const deletedExpenses = localStorage.getItem("tempExpenses") || [];
-  undoLink.textContent = deletedExpenses.length > 0 ? undoDisplayText : "";
-};
-
-const undoLink = document
-  .getElementById("undo-message")
-  .addEventListener("click", (e) => {
-    const tempExpenses = localStorage.getItem("tempExpenses");
-    localStorage.setItem("expenses", tempExpenses);
-    localStorage.removeItem("tempExpenses");
-    location.reload();
-  });
 
 const clearButton = document
   .getElementById("clear-expenses-btn")
@@ -49,19 +33,19 @@ const clearButton = document
     location.reload();
   });
 
-const form = document
+const inputForm = document
   .getElementById("expense-form")
   .addEventListener("submit", () => {
     const date = document.getElementById("expense-date").value;
     const description = document.getElementById("expense-description").value;
     const amount = document.getElementById("expense-amount").value;
     const vendor = document.getElementById("expense-vendor").value;
-    addNewExpense(date, description, amount, vendor);
+    addNewExpense(vendor, description, date, amount);
   });
 
-const addNewExpense = (date, description, amount, vendor) => {
+const addNewExpense = (vendor, description, date, amount) => {
   const id = Date.now() + Math.random();
-  const newExpense = new Expense(id, date, description, amount, vendor);
+  const newExpense = new Expense(vendor, description, date, amount, id);
   setLocalExpenses(newExpense);
 };
 
@@ -79,32 +63,45 @@ const setLocalExpenses = (newExpense) => {
   const savedExpenses =
     JSON.parse(window.localStorage.getItem("expenses")) || [];
   savedExpenses.push(newExpense);
-  displayExpensesNEW(savedExpenses);
+  displayExpenses(savedExpenses);
   localStorage.setItem("expenses", JSON.stringify(savedExpenses));
   localStorage.removeItem("tempExpenses");
 };
 
+
+
+
+
+// Function untested
 const updateExistingExpense = (expense) => {
   const savedExpenses = JSON.parse(localStorage.getItem("expenses")) || [];
   const matchingExpense = savedExpenses.find(
-    (savedExpense) => expense.id === savedExpenses.id
-  );
+    () => expense.id === savedExpenses.id);
+
   const index = savedExpenses.indexOf(matchingExpense);
   savedExpenses[index] = expense;
   window.localStorage.setItem("expenses", JSON.stringify(savedExpenses));
 };
 
+
+
+
+
 const deleteExpense = (expense) => {
   const savedExpenses = JSON.parse(localStorage.getItem("expenses")) || [];
-  const matchingExpense = savedExpenses.find(
-    (savedExpense) => expense.id === savedExpenses.id
-  );
+  const matchingExpense = savedExpenses.find((savedExpense) => {
+    return expense.id === savedExpense.id;
+  });
   const index = savedExpenses.indexOf(matchingExpense);
   if (index > -1) {
     savedExpenses.splice(index, 1);
     localStorage.setItem("expenses", JSON.stringify(savedExpenses));
   }
 };
+
+
+
+
 
 const displayCurrency = (value) => {
   const dec = value.split(".")[1];
@@ -116,7 +113,7 @@ const addCommasToDollars = (value) =>
   value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
 const displayDate = (value) => {
-  const d = new Date(value);
+  const d = new Date(`${value} 00:00:00`);
   const month = new Intl.DateTimeFormat("en", { month: "numeric" }).format(d);
   const day = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(d);
   const year = new Intl.DateTimeFormat("en", { year: "numeric" }).format(d);
@@ -124,8 +121,8 @@ const displayDate = (value) => {
   return dateFormtted;
 };
 
-const displayExpensesNEW = (savedSortedExpenses) => {
-  if (savedSortedExpenses.length > 1) {
+const displayExpenses = (savedSortedExpenses) => {
+  if (savedSortedExpenses.length > 0) {
     let displayTo = document.querySelector("table");
     let expenseHeaders = Object.keys(savedSortedExpenses[0]);
     generateExpensesTable(displayTo, savedSortedExpenses);
@@ -139,31 +136,25 @@ const generateExpensesTableHead = (displayTo, expenseHeaders) => {
   for (let expense of expenseHeaders) {
     let th = document.createElement("th");
     let text = document.createTextNode(expense);
-    let headerText = "";
+    let t = text.textContent;
+    text.textContent = `${t.charAt(0).toUpperCase()}${t.slice(1)}`;
     switch (expense) {
       case "id":
         // Hide ID column
         return;
-        break;
-      case "vendor":
-        headerText = "Vendor";
-        break;
-      case "description":
-        headerText = "Description";
-        break;
-      case "date":
-        headerText = "Date";
+        text.textContent = "ID";
         break;
       case "amount":
-        headerText = "$";
+        text.textContent = "$";
         break;
       default:
     }
-    text.textContent = headerText;
     th.appendChild(text);
     row.appendChild(th);
   }
 };
+
+const appendClassName = (value) => `${value}-format`;
 
 const generateExpensesTable = (displayTo, expenses) => {
   for (let element of expenses) {
@@ -184,43 +175,81 @@ const generateExpensesTable = (displayTo, expenses) => {
       cell.appendChild(text);
     }
   }
+  testingDelete();
 };
 
-const appendClassName = (value) => `${value}-format`;
 
-// Refactor this using document.createElement()
-// .innerHTML is potentially unsafe
-// const displayExpenses = (expenses) => {
-//   var html = "<table border class='expenses-table'>";
-//   html += "<tr>";
-//   html += "<td class='expense-header-row'>Vendor</td>";
-//   html += "<td class='expense-header-row'>Description</td>";
-//   html += "<td class='expense-header-row'>Date</td>";
-//   html += "<td class='expense-header-row'>$</td>";
-//   html +=
-//     "<td class='expense-header-row'><input id='delete-all' type='checkbox' class='check-box'></td></tr>";
-//   for (var i = 0; i < expenses.length; i++) {
-//     html += "<tr>";
-//     html +=
-//       "<td class='expense-data-row vendor-format'>" +
-//       expenses[i].vendor +
-//       "</td>";
-//     html +=
-//       "<td class='expense-data-row description-format'>" +
-//       expenses[i].description +
-//       "</td>";
-//     html +=
-//       "<td class='expense-data-row  date-format'>" +
-//       displayDate(expenses[i].date) +
-//       "</td>";
-//     html +=
-//       "<td class='expense-data-row enMoney'>" +
-//       displayCurrency(expenses[i].amount) +
-//       "</td>";
-//     html +=
-//       "<td class='expense-data-row checkbox-format'><input type='checkbox' class='delete-checkbox'></td>";
-//     html += "</tr>";
-//   }
-//   html += "</table>";
-//   document.getElementById("display-expenses").innerHTML = html;
-// };
+
+const tableRow = document.querySelector('table').addEventListener("click", (e) => {
+  // console.log(e.target.parentElement);
+  // console.log(e.target.parentElement.querySelector('.id-format').textContent);
+  const selectedRow = e.target.parentElement;
+  
+  const selectedRowID = selectedRow.querySelector('.id-format').textContent;
+  const selectedRowDate = selectedRow.querySelector('.date-format').textContent;
+  const selectedRowVendor = selectedRow.querySelector('.vendor-format').textContent;
+  const selectedRowDescription = selectedRow.querySelector('.description-format').textContent;
+  const selectedRowAmount = selectedRow.querySelector('.amount-format').textContent;
+
+  // selectedRowVendor, selectedRowDescription, selectedRowDate, selectedRowAmount, selectedRowID
+  const exp = new Expense(selectedRowVendor, selectedRowDescription, selectedRowDate, selectedRowAmount, selectedRowID);
+  console.log(exp);
+  
+  deleteExpense(exp);
+
+  // console.log(selectedRowID);
+
+  // const savedExpenses = JSON.parse(localStorage.getItem("expenses"));
+  // console.log(savedExpenses);
+
+  // const matchingExpense = savedExpenses.find(() => savedExpenses.id === selectedRowID);
+  // const index = savedExpenses.indexOf(matchingExpense);
+  // if (index > -1) {
+  //   savedExpenses.splice(index, 1);
+  //   localStorage.setItem("expenses", JSON.stringify(savedExpenses));
+  // }
+
+});
+
+
+
+function testingDelete() {
+  var index,
+    table = document.querySelector("table");
+  for (var i = 0; i < table.rows.length; i++) {
+    table.rows[i].onClick = function () {
+      index = this.rowIndex;
+      console.log(index);
+    };
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const checkForUndo = () => {
+  const undoLink = document.getElementById("undo-message");
+  const undoDisplayText = "Expenses Deleted, click here to restore.";
+  const deletedExpenses = localStorage.getItem("tempExpenses") || [];
+  undoLink.textContent = deletedExpenses.length > 0 ? undoDisplayText : "";
+};
+
+const undoLink = document
+  .getElementById("undo-message")
+  .addEventListener("click", (e) => {
+    const tempExpenses = localStorage.getItem("tempExpenses");
+    localStorage.setItem("expenses", tempExpenses);
+    localStorage.removeItem("tempExpenses");
+    location.reload();
+  });
